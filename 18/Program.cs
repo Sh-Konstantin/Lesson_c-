89f5337b4e33df1ff7003fc46b69008b7d9eb8bb
Sh_Konstantin <743442@mail.ru>
2023-01-12 19:59:38 +0300

    study_1

---
+++ 18/Program.cs
@@ -0,0 +1,11 @@
+﻿//программа которая вход принимает число и выдает его квадрат 
+
+int number =10;
+int result1 = number * number;
+Console.WriteLine (result1);
+
+Console.WriteLine();
+
+double numb = 2;
+double result = Math.Pow (numb, 2);
+Console.WriteLine (result);
