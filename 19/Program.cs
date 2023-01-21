80c233dec049d0c6903d79a9b425657c76ae9cd0
Sh_Konstantin <743442@mail.ru>
2023-01-22 00:46:55 +0300

    19

---
+++ 19/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу которая принимает два числа и проверяет является ли первое число квадратом второго
+double number1 = 3;
+double number2 = 25;
+
+if (number1 == Math.Sqrt(number2))      
+    Console.WriteLine ("все верно");
+else
+    Console.WriteLine ("проверь условие");
+
+Console.WriteLine();
+
